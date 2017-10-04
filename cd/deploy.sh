f74f4d7782221fbc4f8b467e380c1ee7a87eba6b
p013570 <p013570@users.noreply.github.com>
2017-10-04 09:14:07 +0100

    Added compiled markdown files and javadoc to gitignore

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -10,6 +10,6 @@ if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" != 'true' ]; the
    git add .
    git commit -a -m "Updated docs" # This will not be pushed back to github
 
-   npm install -g gitbook-cli
-   ./updateGhPages.sh
+   npm install -g gitbook-cli > /dev/null 2>&1
+   ./scripts/updateGhPages.sh
 fi
--- docs/components/tool/deployment.md
+++
@@ -1,5 +0,0 @@
-
-# Deployment Scripts
-
-This module contains deployment scripts and configuration for deploying Gaffer.
-See [gaffer-tools/deployment](https://github.com/gchq/gaffer-tools/tree/master/deployment).
