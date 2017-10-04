6bfe965aa8d1b1a16846803762f11cf45764e6e6
p013570 <p013570@users.noreply.github.com>
2017-10-04 11:40:20 +0100

    Attempt to fix pushing to github

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" != 'true' ]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
-  git remote set-url origin https://${GITHUB_TOKEN}@github.com/gchq/gaffer-doc.git
+  git remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/gchq/gaffer-doc.git
 
   ./scripts/updateGhPages.sh
 fi
