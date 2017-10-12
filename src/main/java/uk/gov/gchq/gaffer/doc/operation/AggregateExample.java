4f972df6545886aacb26fcb619d16c05d48e653d
m607123 <m607123@users.noreply.github.com>
2017-10-12 11:30:13 +0100

    gh-8 - Aggregate examples written

--- src/main/java/uk/gov/gchq/gaffer/doc/operation/AggregateExample.java
+++ src/main/java/uk/gov/gchq/gaffer/doc/operation/AggregateExample.java
@@ -15,22 +15,22 @@
  */
 package uk.gov.gchq.gaffer.doc.operation;
 
-import uk.gov.gchq.gaffer.commonutil.iterable.CloseableIterable;
 import uk.gov.gchq.gaffer.data.element.Element;
 import uk.gov.gchq.gaffer.data.element.function.ElementAggregator;
-import uk.gov.gchq.gaffer.named.operation.NamedOperation;
+import uk.gov.gchq.gaffer.data.element.function.ElementTransformer;
+import uk.gov.gchq.gaffer.data.elementdefinition.view.View;
+import uk.gov.gchq.gaffer.data.elementdefinition.view.ViewElementDefinition;
 import uk.gov.gchq.gaffer.operation.OperationChain;
+import uk.gov.gchq.gaffer.operation.data.EntitySeed;
 import uk.gov.gchq.gaffer.operation.impl.function.Aggregate;
+import uk.gov.gchq.gaffer.operation.impl.get.GetAdjacentIds;
+import uk.gov.gchq.gaffer.operation.impl.get.GetElements;
 import uk.gov.gchq.gaffer.operation.util.AggregatePair;
-import uk.gov.gchq.gaffer.store.schema.Schema;
-import uk.gov.gchq.gaffer.store.schema.SchemaEdgeDefinition;
-import uk.gov.gchq.gaffer.store.schema.SchemaEntityDefinition;
-import uk.gov.gchq.koryphe.impl.binaryoperator.Max;
+import uk.gov.gchq.koryphe.impl.binaryoperator.StringConcat;
 import uk.gov.gchq.koryphe.impl.binaryoperator.Sum;
+import uk.gov.gchq.koryphe.impl.function.ToString;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class AggregateExample extends OperationExample {
@@ -40,126 +40,86 @@ public class AggregateExample extends OperationExample {
     }
 
     public AggregateExample() {
-        super(Aggregate.class);
+        super(Aggregate.class, "Note - these examples are used for demonstration purposes only " +
+                "with the example dataset.");
     }
 
     @Override
     public void runExamples() {
-        aggregateWithSchema();
-        aggregateElementsInMultipleGroups();
+        aggregateElementsAfterTransformToTransientProperty();
         aggregateWithDataInAggregatePair();
     }
 
-    public void aggregateWithSchema() {
+    public Iterable<? extends Element> aggregateElementsAfterTransformToTransientProperty() {
         // ---------------------------------------------------------
-        final Schema schema = new Schema.Builder()
-                .edge("BasicEdge", new SchemaEdgeDefinition.Builder()
-                        .aggregator(new ElementAggregator.Builder()
-                                .select("count")
-                                .execute(new Sum())
-                                .build())
-                        .groupBy("timestamp")
-                        .build())
-                .build();
-
-        final List<Element> input = new ArrayList<>();
-        final Map<String, AggregatePair> edges = new HashMap<>();
-
         final OperationChain<Iterable<? extends Element>> opChain = new OperationChain.Builder()
-                .first(new NamedOperation.Builder<Element, CloseableIterable<Element>>()
-                        .name("2-hop")
-                        .input(input)
+                .first(new GetAdjacentIds.Builder()
+                        .input(new EntitySeed(1))
+                        .build())
+                .then(new GetAdjacentIds.Builder()
+                        .build())
+                .then(new GetAdjacentIds.Builder()
+                        .build())
+                .then(new GetElements.Builder()
+                        .view(new View.Builder()
+                                .edge("edge", new ViewElementDefinition.Builder()
+                                        .transientProperty("newProp", String.class)
+                                        .transformer(new ElementTransformer.Builder()
+                                                .select("count")
+                                                .execute(new ToString())
+                                                .project("newProp")
+                                                .build())
+                                        .build())
+                                .build())
                         .build())
                 .then(new Aggregate.Builder()
-                        .edges(edges)
+                        .edge("edge", new AggregatePair(new ElementAggregator.Builder()
+                                .select("newProp")
+                                .execute(new StringConcat())
+                                .build()))
                         .build())
                 .build();
-
         // ---------------------------------------------------------
 
-        showExample(opChain, "For this example, we have one group of edges defined in the Schema, " +
-                "with an aggregator and groupBy attached. The aggregator then operates on the output " +
-                "iterable of the NamedOperation, itself in turn returning an iterable of aggregated " +
-                "elements.");
+        return runExample(opChain, "This example demonstrates the aggregation of elements " +
+                "after a transform to a transient property has been applied.");
     }
 
-    public void aggregateElementsInMultipleGroups() {
+    public Iterable<? extends Element> aggregateWithDataInAggregatePair() {
         // ---------------------------------------------------------
-        final Schema schema = new Schema.Builder()
-                .edge("BasicEdge", new SchemaEdgeDefinition.Builder()
-                        .aggregator(new ElementAggregator.Builder()
-                                .select("count")
-                                .execute(new Sum())
-                                .build())
-                        .groupBy("timestamp")
-                        .build())
-                .edge("BasicEdge2", new SchemaEdgeDefinition.Builder()
-                        .aggregator(new ElementAggregator.Builder()
-                                .select("turns")
-                                .execute(new Max())
-                                .build())
-                        .groupBy("location")
-                        .build())
-                .build();
-
-        final List<Element> input = new ArrayList<>();
         final Map<String, AggregatePair> edges = new HashMap<>();
-
-        edges.put("BasicEdge", new AggregatePair());
-        edges.put("BasicEdge2", new AggregatePair());
-
-        final OperationChain<Iterable<? extends Element>> opChain = new OperationChain.Builder()
-                .first(new NamedOperation.Builder<Element, CloseableIterable<Element>>()
-                        .name("2-hop")
-                        .input(input)
-                        .build())
-                .then(new Aggregate.Builder()
-                        .edges(edges)
-                        .build())
-                .build();
-        // ---------------------------------------------------------
-
-        showExample(opChain, "Now we have two groups of edges defined, each with their own " +
-                "groupBy and aggregators. The aggregate function will apply only the relevant " +
-                "aggregators to the relevant groups.");
-    }
-
-    public void aggregateWithDataInAggregatePair() {
-        // ---------------------------------------------------------
-        final Schema schema = new Schema.Builder()
-                .entity("BasicEntity", new SchemaEntityDefinition.Builder()
-                        .build())
-                .build();
-
-        final List<Element> input = new ArrayList<>();
-        final Map<String, AggregatePair> entities = new HashMap<>();
-        final Map<String, String> options = new HashMap<>();
 
         final AggregatePair aggregatePair = new AggregatePair(
-                new String[]{"timestamp"},
+                new String[]{},
                 new ElementAggregator.Builder()
                         .select("count")
                         .execute(new Sum())
-                        .select("otherProperty")
-                        .execute(new Max())
                         .build());
 
-        entities.put("BasicEntity", aggregatePair);
+        edges.put("edge", aggregatePair);
 
         final OperationChain<Iterable<? extends Element>> opChain = new OperationChain.Builder()
-                .first(new NamedOperation.Builder<Element, Iterable<? extends Element>>()
-                        .name("flowMeasure")
-                        .input(input)
-                        .options(options)
+                .first(new GetAdjacentIds.Builder()
+                        .input(new EntitySeed(1))
+                        .build())
+                .then(new GetAdjacentIds.Builder()
+                        .build())
+                .then(new GetAdjacentIds.Builder()
+                        .build())
+                .then(new GetElements.Builder()
+                        .view(new View.Builder()
+                                .edge("edge")
+                                .build())
                         .build())
                 .then(new Aggregate.Builder()
-                        .entities(entities)
+                        .edges(edges)
                         .build())
                 .build();
         // ---------------------------------------------------------
 
-        showExample(opChain, "In this example, we define both the groupBy property, and the " +
-                "ElementAggregator in the constructor of the AggregatePair. The pair is matched " +
-                "to an element group upon which it will operate, in the map named \"entities\".");
+        return runExample(opChain, "In this example, we define the ElementAggregator in the " +
+                "constructor of the AggregatePair. The pair is matched to an element group " +
+                "upon which it will operate, in the map named \"edges\". This can be used to " +
+                "aggregate many groups in different ways, in a relatively simple fashion. ");
     }
 }
