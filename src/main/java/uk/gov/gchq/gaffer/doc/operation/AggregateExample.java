c8d3ce38c344cfc9a8afdbb4a10f76adb391f451
p013570 <p013570@users.noreply.github.com>
2017-10-12 14:50:36 +0100

    gh-8 - changed the examples to make them simpler

--- src/main/java/uk/gov/gchq/gaffer/doc/operation/AggregateExample.java
+++ src/main/java/uk/gov/gchq/gaffer/doc/operation/AggregateExample.java
@@ -15,23 +15,10 @@
  */
 package uk.gov.gchq.gaffer.doc.operation;
 
-import uk.gov.gchq.gaffer.data.element.Element;
 import uk.gov.gchq.gaffer.data.element.function.ElementAggregator;
-import uk.gov.gchq.gaffer.data.element.function.ElementTransformer;
-import uk.gov.gchq.gaffer.data.elementdefinition.view.View;
-import uk.gov.gchq.gaffer.data.elementdefinition.view.ViewElementDefinition;
-import uk.gov.gchq.gaffer.operation.OperationChain;
-import uk.gov.gchq.gaffer.operation.data.EntitySeed;
 import uk.gov.gchq.gaffer.operation.impl.function.Aggregate;
-import uk.gov.gchq.gaffer.operation.impl.get.GetAdjacentIds;
-import uk.gov.gchq.gaffer.operation.impl.get.GetElements;
 import uk.gov.gchq.gaffer.operation.util.AggregatePair;
 import uk.gov.gchq.koryphe.impl.binaryoperator.StringConcat;
-import uk.gov.gchq.koryphe.impl.binaryoperator.Sum;
-import uk.gov.gchq.koryphe.impl.function.ToString;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AggregateExample extends OperationExample {
 
@@ -40,86 +27,36 @@ public class AggregateExample extends OperationExample {
     }
 
     public AggregateExample() {
-        super(Aggregate.class, "Note - these examples are used for demonstration purposes only " +
-                "with the example dataset.");
+        super(Aggregate.class, "The Aggregate operation would normally be used in an Operation Chain to aggregate the results of a previous operation.");
     }
 
     @Override
     public void runExamples() {
-        aggregateElementsAfterTransformToTransientProperty();
-        aggregateWithDataInAggregatePair();
+        simpleAggregateElementsExample();
+        aggregateOnlyEdgesOfTypeEdgeWithATransientPropertyAndProvidedAggregator();
     }
 
-    public Iterable<? extends Element> aggregateElementsAfterTransformToTransientProperty() {
+    public void simpleAggregateElementsExample() {
         // ---------------------------------------------------------
-        final OperationChain<Iterable<? extends Element>> opChain = new OperationChain.Builder()
-                .first(new GetAdjacentIds.Builder()
-                        .input(new EntitySeed(1))
-                        .build())
-                .then(new GetAdjacentIds.Builder()
-                        .build())
-                .then(new GetAdjacentIds.Builder()
-                        .build())
-                .then(new GetElements.Builder()
-                        .view(new View.Builder()
-                                .edge("edge", new ViewElementDefinition.Builder()
-                                        .transientProperty("newProp", String.class)
-                                        .transformer(new ElementTransformer.Builder()
-                                                .select("count")
-                                                .execute(new ToString())
-                                                .project("newProp")
-                                                .build())
-                                        .build())
-                                .build())
-                        .build())
-                .then(new Aggregate.Builder()
-                        .edge("edge", new AggregatePair(new ElementAggregator.Builder()
-                                .select("newProp")
+        final Aggregate aggregate = new Aggregate();
+        // ---------------------------------------------------------
+
+        showExample(aggregate, null);
+    }
+
+    public void aggregateOnlyEdgesOfTypeEdgeWithATransientPropertyAndProvidedAggregator() {
+        // ---------------------------------------------------------
+        final String[] groupBy = {};
+        final Aggregate aggregate = new Aggregate.Builder()
+                .edge("edge", new AggregatePair(
+                        groupBy,
+                        new ElementAggregator.Builder()
+                                .select("transientProperty1")
                                 .execute(new StringConcat())
                                 .build()))
-                        .build())
                 .build();
         // ---------------------------------------------------------
 
-        return runExample(opChain, "This example demonstrates the aggregation of elements " +
-                "after a transform to a transient property has been applied.");
-    }
-
-    public Iterable<? extends Element> aggregateWithDataInAggregatePair() {
-        // ---------------------------------------------------------
-        final Map<String, AggregatePair> edges = new HashMap<>();
-
-        final AggregatePair aggregatePair = new AggregatePair(
-                new String[]{},
-                new ElementAggregator.Builder()
-                        .select("count")
-                        .execute(new Sum())
-                        .build());
-
-        edges.put("edge", aggregatePair);
-
-        final OperationChain<Iterable<? extends Element>> opChain = new OperationChain.Builder()
-                .first(new GetAdjacentIds.Builder()
-                        .input(new EntitySeed(1))
-                        .build())
-                .then(new GetAdjacentIds.Builder()
-                        .build())
-                .then(new GetAdjacentIds.Builder()
-                        .build())
-                .then(new GetElements.Builder()
-                        .view(new View.Builder()
-                                .edge("edge")
-                                .build())
-                        .build())
-                .then(new Aggregate.Builder()
-                        .edges(edges)
-                        .build())
-                .build();
-        // ---------------------------------------------------------
-
-        return runExample(opChain, "In this example, we define the ElementAggregator in the " +
-                "constructor of the AggregatePair. The pair is matched to an element group " +
-                "upon which it will operate, in the map named \"edges\". This can be used to " +
-                "aggregate many groups in different ways, in a relatively simple fashion. ");
+        showExample(aggregate, "The groupBy has been set to an empty array. This will override the groupBy value in the schema.");
     }
 }
