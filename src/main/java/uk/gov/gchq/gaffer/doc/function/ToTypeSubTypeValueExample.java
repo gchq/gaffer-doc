7541e5f9377e2c812b09cd930f88a31c528356cc
d47853 <d47853@users.noreply.github.com>
2019-01-24 15:37:39 +0000

    Gh 129 add to TypeSubTypeValue docs (#133)
    * gh-129 added docs for functions
    
    * gh-129 reverted Gaffer version number
    
    * gh-129 added headers
    
    * gh-129 added comments
    
    * gh-129 fixed checkstyle issues
    
    * gh-129 - header updated

---
+++ src/main/java/uk/gov/gchq/gaffer/doc/function/ToTypeSubTypeValueExample.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.gchq.gaffer.doc.function;
+
+import uk.gov.gchq.gaffer.types.TypeValue;
+import uk.gov.gchq.gaffer.types.function.ToTypeSubTypeValue;
+
+import java.util.function.Function;
+
+public class ToTypeSubTypeValueExample extends FunctionExample {
+
+    public ToTypeSubTypeValueExample() {
+        super(ToTypeSubTypeValue.class, "Converts a value into a TypeSubTypeValue");
+    }
+
+    public static void main(final String[] args) {
+        new ToTypeSubTypeValueExample().runAndPrint();
+    }
+
+    @Override
+    protected void runExamples() {
+        toTypeSubTypeValue();
+    }
+
+    private void toTypeSubTypeValue() {
+        // ---------------------------------------------------------
+        Function toTypeSubTypeValue = new ToTypeSubTypeValue();
+        // ---------------------------------------------------------
+
+        super.runExample(
+                toTypeSubTypeValue,
+                null,
+                "aString", 100L, 25, new TypeValue("type1", "value1"), null
+        );
+    }
+}
---
+++ src/main/java/uk/gov/gchq/gaffer/doc/function/ToTypeValueExample.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.gchq.gaffer.doc.function;
+
+import uk.gov.gchq.gaffer.types.TypeValue;
+import uk.gov.gchq.gaffer.types.function.ToTypeValue;
+
+import java.util.function.Function;
+
+public class ToTypeValueExample extends FunctionExample {
+
+    public ToTypeValueExample() {
+        super(ToTypeValue.class, "Converts a value into a TypeValue");
+    }
+
+    public static void main(final String[] args) {
+        new ToTypeValueExample().runAndPrint();
+    }
+
+    @Override
+    protected void runExamples() {
+        toTypeValue();
+    }
+
+    private void toTypeValue() {
+        // ---------------------------------------------------------
+        Function toTypeValue = new ToTypeValue();
+        // ---------------------------------------------------------
+
+        super.runExample(
+                toTypeValue,
+                null,
+                "aString", 100L, 25, new TypeValue("type1", "value1"), null
+        );
+    }
+}
