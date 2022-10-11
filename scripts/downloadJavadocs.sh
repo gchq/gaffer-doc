0e2230fa6ba2fe3eec0628f1524f74543045402e
GCHQDeveloper314 <94527357+GCHQDeveloper314@users.noreply.github.com>
2022-10-11 11:35:02 +0100

    gh-290: Fetch specific versions of Javadocs instead of the latest (#291)
    * Fetch specific versions of Javadocs instead of the latest
    Instead of getting the latest commit from gh-pages, search for the commit for the version required (using the GitHub API) and fetch that version instead. Tidy up the script by moving most duplicate code into a function.
    
    * Test is regex work on MacOS
    
    * Use sed instead of bash regex, use linux runner again
    
    * Use GH token to autenticate with the API

--- scripts/downloadJavadocs.sh
+++ scripts/downloadJavadocs.sh
@@ -8,33 +8,45 @@ korypheVersion=$3
 # Update javadocs
 mkdir -p docs/javadoc
 cd docs/javadoc
+
+fetchJavadoc () {
+    rm -rf $1
+    restUrl='https://api.github.com/repos/gchq/'$1'/commits?sha=gh-pages' #Requests to this API are ratelimited without a token
+    commitShaRegex='.*message.*'$2'.,.*.tree...{.*sha....([[:alnum:]]*).,'
+    if [ -n "$GITHUB_TOKEN" ]; then
+        echo 'GITHUB_TOKEN found, authorising to GitHub API with it'
+        githubCommitHistory=$(curl -Ss -H "Accept: application/vnd.github.v3+json" -H "Authorization: Bearer "$GITHUB_TOKEN $restUrl)
+    else
+        echo 'GITHUB_TOKEN not found, not using GitHub API authorisation'
+        githubCommitHistory=$(curl -Ss -H "Accept: application/vnd.github.v3+json" $restUrl)
+    fi
+    commitSha=$(echo $githubCommitHistory | sed -nE 's/.*Updated javadoc - '$2'.,..tree...\{..sha....([[:alnum:]]*).*/\1/p')
+
+    if [ -z "$commitSha" ]; then
+        echo 'Error finding '$1' Javadoc for version '$2
+        echo 'curl output:'$githubCommitHistory
+        exit 1
+    fi
+    echo 'Found '$1' Javadoc with version '$2
+    curl -Ss -L 'https://github.com/gchq/'$1'/archive/'$commitSha'.zip' > $1.zip
+    unzip -q $1.zip
+    mv *-$commitSha $1
+    rm $1.zip
+    echo '' > $1/$2
+}
+
 if [ ! -f gaffer/$gafferVersion ]; then
-    rm -rf gaffer
-    curl -L https://github.com/gchq/Gaffer/tarball/gh-pages > gaffer.tar.gz
-    tar -xvf gaffer.tar.gz
-    mv gchq-Gaffer-* gaffer
-    rm -rf gaffer.tar.gz
-    echo '' > gaffer/$gafferVersion
+    fetchJavadoc 'gaffer' $gafferVersion
 else
     echo 'Gaffer Javadoc already up to date'
 fi
 if [ ! -f gaffer-tools/$gafferToolsVersion ]; then
-    rm -rf gaffer-tools
-    curl -L https://github.com/gchq/gaffer-tools/tarball/gh-pages > gaffer-tools.tar.gz
-    tar -xvf gaffer-tools.tar.gz
-    mv gchq-gaffer-tools-* gaffer-tools
-    rm -rf gaffer-tools.tar.gz
-    echo '' > gaffer-tools/$gafferToolsVersion
+    fetchJavadoc 'gaffer-tools' $gafferToolsVersion
 else
     echo 'Gaffer Tools Javadoc already up to date'
 fi
 if [ ! -f koryphe/$korypheVersion ]; then
-    rm -rf koryphe
-    curl -L https://github.com/gchq/koryphe/tarball/gh-pages > koryphe.tar.gz
-    tar -xvf koryphe.tar.gz
-    mv gchq-koryphe-* koryphe
-    rm -rf koryphe.tar.gz
-    echo '' > koryphe/$korypheVersion
+    fetchJavadoc 'koryphe' $korypheVersion
 else
     echo 'Koryphe Javadoc already up to date'
 fi
